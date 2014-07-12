16448f6b3e31a80bddd3361f15c661415d034fd3
dmreagan <dmreagan@iu.edu>
2014-07-12 16:18:59 -0400

    Add cache control. Seems to work for some operations, but not all.

--- utilities.php
+++ utilities.php
@@ -1287,6 +1287,10 @@ function create_nav_bar()
  */
 function create_head()
 {
+    header( 'Cache-Control: no-store, no-cache, must-revalidate' );
+    header( 'Cache-Control: post-check=0, pre-check=0', false );
+    header( 'Pragma: no-cache' );
+
     echo'
         <head>
             <title>PHP Reference Gateway</title>
